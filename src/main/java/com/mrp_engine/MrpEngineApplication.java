2acbbc74b704141978be0dfc6cdfc8b047832ece
Ananya-09-AP <ananya09ap@gmail.com>
2026-06-09 18:58:08 +0530

    init commit

---
+++ src/main/java/com/mrp_engine/MrpEngineApplication.java
@@ -0,0 +1,13 @@
+package com.mrp_engine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MrpEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MrpEngineApplication.class, args);
+	}
+
+}
